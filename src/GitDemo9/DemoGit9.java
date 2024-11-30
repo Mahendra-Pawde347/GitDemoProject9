f84e4f204b7082a9c8175becae3822419ff7404b
mahendrapawde <pawdemahendra13@gmail.com>
2024-11-30 17:55:49 +0530

    this is GitProject 9

---
+++ src/GitDemo9/DemoGit9.java
@@ -0,0 +1,12 @@
+package GitDemo9;
+
+public class DemoGit9 
+{
+public static void main(String[] args) 
+{
+System.out.println("Git demo project 9");	
+}
+}
+
+
+//https://github.com/Mahendra-Pawde347/GitDemoProject9.git
